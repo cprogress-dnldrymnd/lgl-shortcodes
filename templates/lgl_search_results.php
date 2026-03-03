3b9763ad38c8951add872caccb782dd68d996180
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-03 20:30:07 +0800

    css

--- templates/lgl_search_results.php
+++ templates/lgl_search_results.php
@@ -5,20 +5,20 @@
  */
 ?>
 <div class="bt-elwg-cars-grid--default lgl-results-wrapper">
-    <div class="bt-cars-grid-list-template">
+    <div class="lgls-grid-list-template">
         <div class="bt-filter-scroll-pos"></div>
-        <div class="bt-car-topbar">
-            <div class="bt-car-col-left">
-                <div class="bt-car-results-block" id="lgl-results-count">
+        <div class="lgl-topbar">
+            <div class="lgl-col-left">
+                <div class="lgl-results-block" id="lgl-results-count">
                     Awaiting Search...
                 </div>
             </div>
 
-            <div class="bt-car-col-right">
-                <form class="bt-car-filter-form-sortview" id="lgl-sort-form" action="" method="get">
+            <div class="lgl-col-right">
+                <form class="lgl-filter-form-sortview" id="lgl-sort-form" action="" method="get">
                     <input type="hidden" name="orderby" value="date">
                     <input type="hidden" name="order" value="desc">
-                    <div class="bt-car-sort-block">
+                    <div class="lgl-sort-block">
                         <span class="bt-sort-title">Sort by:</span>
                         <div class="bt-sort-field">
                             <select name="sort_order" id="lgl-sort-order" class="lgl-select2" style="width: 220px;">
@@ -37,9 +37,9 @@
         
         <div class="bt-filter-results">
             <span class="bt-loading-wave" id="lgl-loader" style="display: none;">Loading...</span>
-            <div class="bt-car-grid-layout bt-cols--3 bt-layout-default" id="lgl-results-grid" data-limit="9">
+            <div class="lgl-grid-layout bt-cols--3 bt-layout-default" id="lgl-results-grid" data-limit="9">
                 </div>
-            <div class="bt-car-pagination-wrap"></div>
+            <div class="lgl-pagination-wrap"></div>
         </div>
     </div>
 </div>
