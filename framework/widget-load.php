e2a66ab87fbf6829d7a76f7c6c918eee23d23b58
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-03 18:08:20 +0800

    Update widget-load.php

--- framework/widget-load.php
+++ framework/widget-load.php
@@ -13,116 +13,115 @@ namespace LGLArtElementorWidgets;
 class ElementorWidgets
 {
 
-    private static $_instance = null;
+	private static $_instance = null;
 
-    public static function instance()
-    {
-        if (is_null(self::$_instance)) {
-            self::$_instance = new self();
-        }
-        return self::$_instance;
-    }
+	public static function instance()
+	{
+		if (is_null(self::$_instance)) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
 
-    public $widgets = array();
+	public $widgets = array();
 
-    public function widgets_list()
-    {
-        // FIX: Ensure these match the EXACT folder names in /framework/widgets/
-        $this->widgets = array(
-            'car-loop-item', // Changed from 'Widget_CarLoopItem' to match standard naming
-            'cars-compare',
-            'cars-quick-compare',
-            'cars-search',
-            'cars-search-style-1',
-            'cars-search-style-2',
-            'car-loop-item-style-1',
-            'car-loop-item-style-2',
-            'car-loop-item-style-3',
-            'car-loop-item-style-4',
-            'cars-grid-list',
-            'cars-grid',
-            'cars-wishlist' 
-        );
+	public function widgets_list()
+	{
+		// FIX: Ensure these match the EXACT folder names in /framework/widgets/
+		$this->widgets = array(
+			'car-loop-item', // Changed from 'Widget_CarLoopItem' to match standard naming
+			'cars-compare',
+			'cars-quick-compare',
+			'cars-search',
+			'cars-search-style-1',
+			'cars-search-style-2',
+			'car-loop-item-style-1',
+			'car-loop-item-style-2',
+			'car-loop-item-style-3',
+			'car-loop-item-style-4',
+			'cars-grid-list',
+			'cars-grid',
+			'cars-wishlist'
+		);
 
-        return $this->widgets;
-    }
+		return $this->widgets;
+	}
 
-    public function widget_styles()
-    {
-        wp_enqueue_style('slick-slider', get_template_directory_uri() . '/assets/libs/slick/slick.css', array(), false);
-    }
+	public function widget_styles()
+	{
+		wp_enqueue_style('slick-slider', get_template_directory_uri() . '/assets/libs/slick/slick.css', array(), false);
+	}
 
-    public function widget_scripts()
-    {
-        wp_register_script('slick-slider', get_template_directory_uri() . '/assets/libs/slick/slick.min.js', array('jquery'), '', true);
-        wp_register_script('select2-min', get_template_directory_uri() . '/assets/libs/select2/select2.min.js', array('jquery'), '', true);
-        wp_register_script('elementor-widgets', get_template_directory_uri() . '/framework/widgets/frontend.js', ['jquery'], '', true);
-    }
+	public function widget_scripts()
+	{
+		wp_register_script('slick-slider', get_template_directory_uri() . '/assets/libs/slick/slick.min.js', array('jquery'), '', true);
+		wp_register_script('select2-min', get_template_directory_uri() . '/assets/libs/select2/select2.min.js', array('jquery'), '', true);
+		wp_register_script('elementor-widgets', get_template_directory_uri() . '/framework/widgets/frontend.js', ['jquery'], '', true);
+	}
 
-    private function include_widgets_files()
-    {
-        foreach ($this->widgets_list() as $widget) {
-            $widget_file = get_template_directory() . '/framework/widgets/' . $widget . '/widget.php';
-            
-            if ( file_exists( $widget_file ) ) {
-                require_once $widget_file;
-            }
+	private function include_widgets_files()
+	{
+		foreach ($this->widgets_list() as $widget) {
+			$widget_file = get_template_directory() . '/framework/widgets/' . $widget . '/widget.php';
 
-            $skins_pattern = get_template_directory() . '/framework/widgets/' . $widget . '/skins/*.php';
-            foreach (glob($skins_pattern) as $filepath) {
-                if ( file_exists( $filepath ) ) {
-                    include $filepath;
-                }
-            }
-        }
-    }
+			if (file_exists($widget_file)) {
+				require_once $widget_file;
+			}
 
-    public function register_categories($elements_manager)
-    {
-        $elements_manager->add_category(
-            'clwyd',
-            [
-                'title' => esc_html__('Clwyd', 'clwyd')
-            ]
-        );
-    }
+			$skins_pattern = get_template_directory() . '/framework/widgets/' . $widget . '/skins/*.php';
+			foreach (glob($skins_pattern) as $filepath) {
+				if (file_exists($filepath)) {
+					include $filepath;
+				}
+			}
+		}
+	}
 
-    public function register_widgets($widgets_manager)
-    {
-        $this->include_widgets_files();
+	public function register_categories($elements_manager)
+	{
+		$elements_manager->add_category(
+			'clwyd',
+			[
+				'title' => esc_html__('Clwyd', 'clwyd')
+			]
+		);
+	}
 
-        // FIX: Using fully qualified namespaces to guarantee the classes are found.
-        // Ensure that the namespace defined at the top of each widget.php matches these exactly.
-        
-        if ( class_exists( '\LGLArtElementorWidgets\Widgets\CarLoopItem\Widget_CarLoopItem' ) ) {
-             $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItem\Widget_CarLoopItem());
-        }
-        
-        // Uncomment and update the following lines as you verify the namespaces in each individual widget file
-        
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItemStyle1\Widget_CarLoopItemStyle1());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItemStyle2\Widget_CarLoopItemStyle2());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItemStyle3\Widget_CarLoopItemStyle3());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItemStyle4\Widget_CarLoopItemStyle4());
+	public function register_widgets($widgets_manager)
+	{
+		$this->include_widgets_files();
 
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsCompare\Widget_CarsCompare());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsGrid\Widget_CarsGrid());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsGridList\Widget_CarsGridList());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsQuickCompare\Widget_CarsQuickCompare());
+		// FIX: Using fully qualified namespaces to guarantee the classes are found.
+		// Ensure that the namespace defined at the top of each widget.php matches these exactly.
 
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsSearch\Widget_CarsSearch());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsSearchStyle1\Widget_CarsSearchStyle1());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsSearchStyle2\Widget_CarsSearchStyle2());
-        // $widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsWishlist\Widget_CarsWishlist());
-    }
+		if (class_exists('\LGLArtElementorWidgets\Widgets\CarLoopItem\Widget_CarLoopItem')) {
+			$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItem\Widget_CarLoopItem());
+		}
 
-    public function __construct()
-    {
-        add_action('elementor/frontend/after_register_styles', [$this, 'widget_styles']);
-        add_action('elementor/frontend/after_register_scripts', [$this, 'widget_scripts']);
-        add_action('elementor/elements/categories_registered', [$this, 'register_categories']);
-        add_action('elementor/widgets/register', [$this, 'register_widgets']);
-    }
+
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItemStyle1\Widget_CarLoopItemStyle1());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItemStyle2\Widget_CarLoopItemStyle2());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItemStyle3\Widget_CarLoopItemStyle3());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarLoopItemStyle4\Widget_CarLoopItemStyle4());
+
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsCompare\Widget_CarsCompare());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsGrid\Widget_CarsGrid());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsGridList\Widget_CarsGridList());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsQuickCompare\Widget_CarsQuickCompare());
+
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsSearch\Widget_CarsSearch());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsSearchStyle1\Widget_CarsSearchStyle1());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsSearchStyle2\Widget_CarsSearchStyle2());
+		$widgets_manager->register(new \LGLArtElementorWidgets\Widgets\CarsWishlist\Widget_CarsWishlist());
+	}
+
+	public function __construct()
+	{
+		add_action('elementor/frontend/after_register_styles', [$this, 'widget_styles']);
+		add_action('elementor/frontend/after_register_scripts', [$this, 'widget_scripts']);
+		add_action('elementor/elements/categories_registered', [$this, 'register_categories']);
+		add_action('elementor/widgets/register', [$this, 'register_widgets']);
+	}
 }
 
 // Instantiate ElementorWidgets Class
