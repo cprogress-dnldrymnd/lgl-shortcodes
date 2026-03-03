5a841226c122d606c522fff9543d8d1198bfb99e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-03 21:25:39 +0800

    css

--- lgl-shortcodes.php
+++ lgl-shortcodes.php
@@ -17,7 +17,7 @@ if (! defined('ABSPATH')) {
 // Define a constant for the plugin directory path to ensure reliable file inclusion.
 define('LGL_SHORTCODES_PATH', plugin_dir_path(__FILE__));
 define('LGL_SHORTCODES_URL', plugin_dir_url(__FILE__));
-define('LGL_SHORTCODES_VERSION', '1.5.1');
+define('LGL_SHORTCODES_VERSION', '1.5.2');
 
 if (! class_exists('LGL_Shortcodes')) {
 
