72f8a64b3ae867780ccb46bf3c415f81c9866a77
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-07 01:00:59 +0800

    Update lgl_listing_grid.php

--- templates/shortcodes/lgl_listing_grid.php
+++ templates/shortcodes/lgl_listing_grid.php
@@ -5,8 +5,6 @@ if ($post_type) {
         paged: 1,
         posts_per_page: 6
     );
-    echo $results['post_type'];
-    echo 'xx2';
     echo '<div class="lgl-grid-layout lgl-cols--3 lgl-layout-default" id="lgl-results-grid">';
     echo $results['html'];
     echo '</div>';
