5caa8165a4c5fe859b5fd7716e7fd47d3f3577e5
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-09 23:49:55 +0800

    Update lgl_wishlist.php

--- templates/shortcodes/lgl_wishlist.php
+++ templates/shortcodes/lgl_wishlist.php
@@ -44,49 +44,7 @@ $wishlist = is_array($wishlist) ? array_values(array_filter(array_map('intval', 
     <?php else : ?>
 
         <div class="lgl-mini-wishlist-content lgl-wishlist-page__list" id="lgl-wishlist-page-list">
-            <ul class="lgl-mini-wishlist-items">
-                <?php foreach ($wishlist as $post_id) :
-                    $post = get_post($post_id);
-                    if (!$post || $post->post_status !== 'publish') continue;
-
-                    $link            = get_permalink($post_id);
-                    $title           = get_the_title($post_id);
-                    $price           = get_post_meta($post_id, 'price', true);
-                    $formatted_price = $price ? '$' . number_format((float) $price, 0) : '';
-                ?>
-                    <li class="lgl-wishlist-item" data-post-id="<?php echo esc_attr($post_id); ?>">
-
-                        <div class="lgl-wishlist-thumb">
-                            <a href="<?php echo esc_url($link); ?>" tabindex="-1" aria-hidden="true">
-                                <?php if (has_post_thumbnail($post_id)) : ?>
-                                    <?php echo get_the_post_thumbnail($post_id, 'thumbnail'); ?>
-                                <?php endif; ?>
-                            </a>
-                        </div>
-
-                        <div class="lgl-wishlist-info">
-                            <h4 class="lgl-wishlist-title">
-                                <a href="<?php echo esc_url($link); ?>"><?php echo esc_html($title); ?></a>
-                            </h4>
-                            <?php if ($formatted_price) : ?>
-                                <span class="lgl-wishlist-price"><?php echo esc_html($formatted_price); ?></span>
-                            <?php endif; ?>
-                        </div>
-
-                        <div class="lgl-wishlist-remove">
-                            <button
-                                class="lgl-remove-btn"
-                                data-id="<?php echo esc_attr($post_id); ?>"
-                                aria-label="<?php esc_attr_e('Remove from wishlist', 'lgl-shortcodes'); ?>">
-                                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#999" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
-                                    <path d="M3 6h18M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2M10 11v6M14 11v6" />
-                                </svg>
-                            </button>
-                        </div>
-
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+            <?php echo $this->get_mini_wishlist_html(); ?>
         </div>
 
     <?php endif; ?>
