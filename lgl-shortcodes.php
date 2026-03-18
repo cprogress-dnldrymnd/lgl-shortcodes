83e6108f98f3ba713ca8aa80b93320a55e6b8c95
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-19 00:46:14 +0800

    Update lgl-shortcodes.php

--- lgl-shortcodes.php
+++ lgl-shortcodes.php
@@ -17,7 +17,7 @@ if (! defined('ABSPATH')) {
 // Define a constant for the plugin directory path to ensure reliable file inclusion.
 define('LGL_SHORTCODES_PATH', plugin_dir_path(__FILE__));
 define('LGL_SHORTCODES_URL', plugin_dir_url(__FILE__));
-define('LGL_SHORTCODES_VERSION', '3.3.9'); // Update this version number with each release for cache busting.
+define('LGL_SHORTCODES_VERSION', '3.4.0'); // Update this version number with each release for cache busting.
 
 if (! class_exists('LGL_Shortcodes')) {
 
