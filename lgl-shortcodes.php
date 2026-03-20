0eb8d711664665fef3ef41ae4e64c94ae901199b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-20 19:41:12 +0800

    Update lgl-shortcodes.php

--- lgl-shortcodes.php
+++ lgl-shortcodes.php
@@ -17,7 +17,7 @@ if (! defined('ABSPATH')) {
 // Define a constant for the plugin directory path to ensure reliable file inclusion.
 define('LGL_SHORTCODES_PATH', plugin_dir_path(__FILE__));
 define('LGL_SHORTCODES_URL', plugin_dir_url(__FILE__));
-define('LGL_SHORTCODES_VERSION', '4.0.6');
+define('LGL_SHORTCODES_VERSION', '4.0.7');
 // ── Load the Forms integration ──
 require_once LGL_SHORTCODES_PATH . 'includes/class-lgl-forms.php';
 require_once LGL_SHORTCODES_PATH . 'includes/class-lgl-email-builder.php';
